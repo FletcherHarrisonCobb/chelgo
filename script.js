08b8ecad441e378431ddcddf55e2e795ddbeff46
FlamingSpeed <126803492+FlamingSpeed@users.noreply.github.com>
2023-11-26 21:29:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function removeJump(){
     character.classList.remove("animate");
 }
 if(character.classList == "animate"){return;}
+var block = document.getElementById("block");
+function checkDead(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        alert("Game over");
+    }
+}
+
+setInterval(checkDead, 10);
